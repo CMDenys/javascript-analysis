efc7cf92b1a3ccb07ad7e97899c7b3d9eda9ed54
Charlie Denys <charlie@Charlies-MBP.hsd1.il.comcast.net>
2021-03-23 14:28:43 -0500

    html table using d3 completed

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,24 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Console.log the weather data from data.js
+console.log(data);
+
+// Step 1: Loop Through `data` and console.log each weather report object
+data.forEach(function(sightingsReport) {
+    console.log(sightingsReport);
+    // Step 2: Use d3 to append one table row `tr` for each weather report object
+    var row = tbody.append("tr")
+    // Step 3: Use `Object.entries` to console.log each weather report value
+    Object.entries(sightingsReport).forEach(function([key, value]) {
+        console.log(key, value);
+        // Step 4: Use d3 to append 1 cell per sightings report value
+        //(datetime, city, state, country, shape, durationMinutes, comments)   
+        var cell = row.append("td");
+        // Step 5: Use d3 to update each cell's text with sightings values
+        cell.text(value);
+    });
+});
