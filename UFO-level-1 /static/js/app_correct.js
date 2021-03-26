6d67d687d15f2bc6f637fd8e18f3493c43096fc4
Charlie Denys <charlie@Charlies-MBP.hsd1.il.comcast.net>
2021-03-25 20:39:23 -0500

    few more changes

--- UFO-level-1 /static/js/app_correct.js
+++ UFO-level-1 /static/js/app_correct.js
@@ -5,18 +5,18 @@ const tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the weather data from data.js
+// Console.log the sightings data from data.js
 //console.log(data);
 
 function buildTable(data) {
 
     tbody.html("");
-    // Step 1: Loop Through `data` and console.log each weather report object
+    // Step 1: Loop Through `data` and console.log each sightings report object
     data.forEach(function(sightingsReport) {
         //console.log(sightingsReport);
-        // Step 2: Use d3 to append one table row `tr` for each weather report object
+        // Step 2: Use d3 to append one table row `tr` for each sightings report object
         var row = tbody.append("tr")
-        // Step 3: Use `Object.entries` to console.log each weather report value
+        // Step 3: Use `Object.entries` to console.log each sightings report value
         Object.entries(sightingsReport).forEach(function([key, value]) {
             //console.log(key, value);
             // Step 4: Use d3 to append 1 cell per sightings report value
